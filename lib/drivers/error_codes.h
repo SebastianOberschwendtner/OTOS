e2369878e3fa96b2e44fe10d84b6cbcdbd3d6824
Sebastian Oberschwendtner <sebastian.oberschwendtner@gmail.com>
2021-09-08 20:03:43 +0200

    :art: Initial commit of error codes.

---
+++ lib/drivers/error_codes.h
@@ -0,0 +1,36 @@
+/**
+ * OTOS - Open Tec Operating System
+ * Copyright (c) 2021 Sebastian Oberschwendtner, sebastian.oberschwendtner@gmail.com
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef ERROR_CODES_H_
+#define ERROR_CODES_H_
+
+// === Includes ===
+
+// === Define the error codes ===
+namespace Error {
+    enum Code_t: int
+    {
+        None                = 0,
+        I2C_Timeout         = -100,
+        I2C_Address_Error   = -101
+    };
+};
+
+#endif
