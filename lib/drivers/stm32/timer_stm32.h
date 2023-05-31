471a73403b93edf221bf99a790aba18f95c7ac3f
Sebastian Oberschwendtner <sebastian.oberschwendtner@gmail.com>
2023-05-31 18:32:22 +0200

    :art: Adds the first timer draft. Works on #7.

--- lib/drivers/stm32/timer_stm32.h
+++ lib/drivers/stm32/timer_stm32.h
@@ -22,13 +22,18 @@
 #define TIMER_STM32_H_
 
 // === Includes ===
-#include "vendors.h"
 #include "interface.h"
+#include "misc/bits.h"
+#include "misc/units.h"
+#include "vendors.h"
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include <ratio>
 
 // === Declarations ===
 namespace Timer
 {
-
     // === Enums ===
     enum class Mode
     {
@@ -39,22 +44,94 @@ namespace Timer
     // === Classes ===
     class Timer
     {
-    private:
+      private:
         // properties
         volatile TIM_TypeDef *thisTimer;
         const IO thisInstance;
+        OTOS::hertz f_tick{F_CPU};
 
-    public:
+        /**
+         * @brief Class representing a timer channel.
+         * The class keeps are reference to the timer and sets its
+         * capture/compare channel related properties.
+         */
+        class Channel
+        {
+          private:
+            Timer *const theTimer;
+            const uint8_t thisChannel{0};
+            volatile uint32_t &compareValue;
+
+          public:
+            // === Constructor ===
+            Channel() = delete;
+            Channel(const uint8_t channel, Timer *const timer, volatile uint32_t &CompareRegister)
+                : theTimer(timer), thisChannel(channel), compareValue(CompareRegister)
+            {
+            }
+
+            // === Methods ===
+            void enable() { this->theTimer->enable_channel(this->thisChannel); }
+            void disable() { this->theTimer->disable_channel(this->thisChannel); }
+            void set_mode(Mode mode) { this->theTimer->set_channel(this->thisChannel, mode); }
+            void set_compare_value(const uint32_t value) { this->compareValue = value; }
+            template <typename rep, typename period>
+            void set_pulse_width(const std::chrono::duration<rep, period> duration)
+            {
+                // calculate the compare value and match the ratios
+                rep calc_value = duration.count() * this->theTimer->f_tick.count();
+                calc_value *= period::num;
+                calc_value /= period::den;
+                this->compareValue = calc_value;
+            }
+            void set_duty_cycle(const float percentage)
+            {
+                // check if percentage is in range
+                if (percentage > 1.0f || percentage < 0.0f)
+                    return;
+                this->compareValue = static_cast<uint32_t>(percentage * this->theTimer->thisTimer->ARR);
+            }
+        };
+
+      public:
         // === Constructor ===
         Timer() = delete;
         Timer(const IO timer);
 
         // === Methods ===
-        unsigned int get_count(void) const;
+        [[nodiscard]] auto is_running() const -> bool;
+        [[nodiscard]] auto get_count() const -> uint32_t;
+        [[nodiscard]] auto get_channel(uint8_t channel) -> Channel;
+        void start();
+        void stop();
+        void set_frequency(OTOS::hertz frequency);
+        void set_top_value(uint32_t top_value);
+        void set_channel(uint8_t channel, Mode mode);
+        void enable_channel(uint8_t channel);
+        void disable_channel(uint8_t channel);
+
+        /**
+         * @brief Set the duration of the timer until overflow/underflow.
+         *
+         * @tparam rep The representation type of the duration.
+         * @tparam period The ratio defining the period of the duration.
+         * @param duration The duration value of the timer until it overflows/underflows.
+         */
+        template <typename rep, typename period>
+        void set_duration(std::chrono::duration<rep, period> duration)
+        {
+            // calculate the top value
+            rep top_value = duration.count() * f_tick.count();
+            top_value /= period::den;
+            top_value *= period::num;
+
+            // set the top value
+            this->set_top_value(static_cast<uint32_t>(top_value));
+        }
     };
 
     // === Functions ===
-    void SysTick_Configure(void);
+    void SysTick_Configure();
 };
 
 #endif
--- test/stm32/test_timer/test_stm32_timer.cpp
+++ test/stm32/test_timer/test_stm32_timer.cpp
@@ -21,7 +21,7 @@
  ******************************************************************************
  * @file     test_timer_stm32.cpp
  * @author  SO
- * @version v2.7.3
+ * @version v4.2.0
  * @date    31-October-2021
  * @brief   Unit tests for testing the timer driver for stm32 microcontrollers.
  ******************************************************************************
@@ -33,18 +33,17 @@
 #include "stm32/timer_stm32.h"
 
 /** === Test List ===
- * ▢ Timer instance can be created:
- *      ▢ clock for peripheral gets enabled
+ * ✓ Timer instance can be created:
+ *      ✓ clock for peripheral gets enabled
  * ✓ Timer count can be read
- * ▢ Timer can be started
- * ▢ Timer can be stopped
- * ▢ The top value (reload value) can be set
+ * ✓ Timer can be started
+ * ✓ Timer can be stopped
+ * ✓ The top value (reload value) can be set
  * ▢ The compare match value can be set
- * ▢ The timer prescaler can be set to match a certain rate
+ * ✓ The timer prescaler can be set to match a certain rate
  * ▢ The timer mode can be read
- * ▢ The timer mode can be set:
- *      ▢ Counter Mode
- *      ▢ PWM Mode
+ * ▢ The mode of the timer channels can be set:
+ *      ▢ PWM Output Mode
  *      ▢ Input Capture Mode
  * ▢ I/O Pins can be assigned to the timer
 */
@@ -73,6 +72,7 @@ void test_init(void)
     Timer::Timer UUT(IO::TIM_1);
 
     // perform tests
+    TEST_ASSERT_BIT_HIGH(0, RCC->APB2ENR);
 };
 
 /// @brief Test the SysTick configuration
@@ -107,6 +107,180 @@ void test_get_count(void)
     TEST_ASSERT_EQUAL(1, UUT.get_count());
 };
 
+/// @brief Test enabling and disabling the timer
+void test_enable_disable(void)
+{
+    // Set stuff up
+    setUp();
+
+    // Create timer
+    Timer::Timer UUT(IO::TIM_1);
+
+    // Test whether timer is disabled by default
+    TEST_ASSERT_FALSE(UUT.is_running());
+
+    // Enable timer
+    UUT.start();
+
+    // Test whether timer is enabled
+    TEST_ASSERT_BIT_HIGH(0, TIM1->CR1);
+    TEST_ASSERT_TRUE(UUT.is_running());
+
+    // Disable timer
+    UUT.stop();
+
+    // Test whether timer is disabled
+    TEST_ASSERT_BIT_LOW(0, TIM1->CR1);
+    TEST_ASSERT_FALSE(UUT.is_running());
+};
+
+/// @brief Test setting the counter frequency
+void test_set_frequency(void)
+{
+    using namespace OTOS::literals;
+    // Set stuff up
+    setUp();
+
+    // Create timer
+    Timer::Timer UUT(IO::TIM_1);
+    Timer::Timer UUT2(IO::TIM_2);
+
+    // Set frequency
+    UUT.set_frequency(1_kHz);
+    UUT2.set_frequency(5_kHz);
+
+    // Test whether the correct value was written to the register
+    uint32_t expected = F_APB2 / 1000 - 1;
+    TEST_ASSERT_EQUAL(expected, TIM1->PSC);
+    expected = F_APB1 / 5000 - 1;
+    TEST_ASSERT_EQUAL(expected, TIM2->PSC);
+};
+
+/// @brief Test setting the top value
+void test_set_top_value(void)
+{
+    using namespace std::literals::chrono_literals;
+    using namespace  OTOS::literals;
+
+    // Set stuff up
+    setUp();
+
+    // Create timer
+    Timer::Timer UUT(IO::TIM_1);
+    UUT.set_frequency(1_kHz);
+
+    // Set top value
+    UUT.set_top_value(100);
+    TEST_ASSERT_EQUAL(100, TIM1->ARR);
+
+    // Set top value as a duration
+    UUT.set_duration(1s);
+    TEST_ASSERT_EQUAL(1000, TIM1->ARR);
+    UUT.set_duration(1ms);
+    TEST_ASSERT_EQUAL(1, TIM1->ARR);
+};
+
+/// @brief Test setting the compare mode
+void test_set_compare_mode(void)
+{
+    // Set stuff up
+    setUp();
+
+    // Create timer
+    Timer::Timer UUT(IO::TIM_1);
+
+    // Set compare mode
+    auto ch1 = UUT.get_channel(1);
+    ch1.set_mode(Timer::Mode::PWM);
+    // UUT.set_channel(1, Timer::Mode::PWM);
+    TEST_ASSERT_BITS_LOW(0b11, TIM1->CCMR1);
+    TEST_ASSERT_BITS((0b111<<4), (0b110<<4), TIM1->CCMR1);
+    UUT.set_channel(2, Timer::Mode::PWM);
+    TEST_ASSERT_BITS_LOW(0b11 << 8, TIM1->CCMR1);
+    TEST_ASSERT_BITS((0b111<<12), (0b110<<12), TIM1->CCMR1);
+    UUT.set_channel(3, Timer::Mode::PWM);
+    TEST_ASSERT_BITS_LOW(0b11, TIM1->CCMR2);
+    TEST_ASSERT_BITS((0b111<<4), (0b110<<4), TIM1->CCMR2);
+    UUT.set_channel(4, Timer::Mode::PWM);
+    TEST_ASSERT_BITS_LOW(0b11 << 8, TIM1->CCMR2);
+    TEST_ASSERT_BITS((0b111<<12), (0b110<<12), TIM1->CCMR2);
+};
+
+/// @brief Test enabling and disabling the timer channels
+void test_enable_disable_channel(void)
+{
+    // Set stuff up
+    setUp();
+
+    // Create timer
+    Timer::Timer UUT(IO::TIM_1);
+
+    // Enable output compare channels
+    UUT.enable_channel(1);
+    TEST_ASSERT_BIT_HIGH(TIM_CCER_CC1E_Pos, TIM1->CCER);
+    UUT.enable_channel(2);
+    TEST_ASSERT_BIT_HIGH(TIM_CCER_CC2E_Pos, TIM1->CCER);
+    UUT.enable_channel(3);
+    TEST_ASSERT_BIT_HIGH(TIM_CCER_CC3E_Pos, TIM1->CCER);
+    UUT.enable_channel(4);
+    TEST_ASSERT_BIT_HIGH(TIM_CCER_CC4E_Pos, TIM1->CCER);
+
+    // Disable output compare channels
+    UUT.disable_channel(1);
+    TEST_ASSERT_BIT_LOW(TIM_CCER_CC1E_Pos, TIM1->CCER);
+    UUT.disable_channel(2);
+    TEST_ASSERT_BIT_LOW(TIM_CCER_CC2E_Pos, TIM1->CCER);
+    UUT.disable_channel(3);
+    TEST_ASSERT_BIT_LOW(TIM_CCER_CC3E_Pos, TIM1->CCER);
+    UUT.disable_channel(4);
+    TEST_ASSERT_BIT_LOW(TIM_CCER_CC4E_Pos, TIM1->CCER);
+
+    // Enable channels via the channel object
+    auto ch1 = UUT.get_channel(1);
+    ch1.enable();
+    TEST_ASSERT_BIT_HIGH(TIM_CCER_CC1E_Pos, TIM1->CCER);
+    ch1.disable();
+    TEST_ASSERT_BIT_LOW(TIM_CCER_CC1E_Pos, TIM1->CCER);
+};
+
+/// @brief Test setting the compare values
+void test_set_compare_value(void)
+{
+    using namespace OTOS::literals;
+    using namespace std::literals::chrono_literals;
+    // Set stuff up
+    setUp();
+
+    // Create timer
+    Timer::Timer UUT(IO::TIM_1);
+
+    // Set compare values as ticks
+    auto ch1 = UUT.get_channel(1);
+    ch1.set_compare_value(100U);
+    TEST_ASSERT_EQUAL(100, TIM1->CCR1);
+
+    // Set compare values as a percentage
+    UUT.set_top_value(1000);
+    ch1.set_duty_cycle(0.5f);
+    TEST_ASSERT_EQUAL(500, TIM1->CCR1);
+    ch1.set_duty_cycle(0.0f);
+    TEST_ASSERT_EQUAL(0, TIM1->CCR1);
+    ch1.set_duty_cycle(1.0f);
+    TEST_ASSERT_EQUAL(1000, TIM1->CCR1);
+    ch1.set_duty_cycle(1.1f);
+    TEST_ASSERT_EQUAL(1000, TIM1->CCR1);
+    ch1.set_duty_cycle(-0.1f);
+    TEST_ASSERT_EQUAL(1000, TIM1->CCR1);
+
+    // Set compare values as a duration
+    UUT.set_frequency(1_kHz);
+    ch1.set_pulse_width(50ms);
+    TEST_ASSERT_EQUAL(50, TIM1->CCR1);
+    UUT.set_frequency(1_Hz);
+    ch1.set_pulse_width(1min);
+    TEST_ASSERT_EQUAL(60, TIM1->CCR1);
+};
+
 // === Main ===
 int main(int argc, char **argv)
 {
@@ -114,5 +288,11 @@ int main(int argc, char **argv)
     RUN_TEST(test_init);
     RUN_TEST(test_configure_SysTick);
     RUN_TEST(test_get_count);
+    RUN_TEST(test_enable_disable);
+    RUN_TEST(test_set_frequency);
+    RUN_TEST(test_set_top_value);
+    RUN_TEST(test_set_compare_mode);
+    RUN_TEST(test_enable_disable_channel);
+    RUN_TEST(test_set_compare_value);
     return UNITY_END();
 };
